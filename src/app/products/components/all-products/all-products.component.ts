4d9c78ba89a2c79fc0b4cf426d42a71bcdb89877
M00Alaa <moalaa641@gmail.com>
2023-09-07 02:00:36 +0300

    finish all products component

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -8,18 +8,50 @@ import { ProductsService } from '../../services/products.service';
 })
 export class AllProductsComponent implements OnInit {
   products: any[] = []
+  categories: any[] = []
 
   constructor (private _ProductsService:ProductsService) {}
 
   ngOnInit():void {
-    this.getAllProducts()
+    this.getAllProducts();
+    this.getAllCategories();
   }
   
   getAllProducts() {
     this._ProductsService.getAllProducts().subscribe((res:any) => {
       this.products = res
       console.log(this.products);
-      
-    })
+    },
+    err => {alert(err.message);
+    }
+    )
   }
+
+  getAllCategories() {
+    this._ProductsService.getAllCategories().subscribe((res:any) => {
+      this.categories = res
+      console.log(this.categories);
+    },
+    err => {alert(err.message);
+    }
+    )
+  }
+  
+  filterCategories(event:any) {
+    let value = event.target.value
+    console.log(value);
+
+    (value == 'all') ? this.getAllProducts() : this.getCategoryProducts(value)
+  }
+
+  getCategoryProducts(categoryName:any) {
+    this._ProductsService.getCategoryProducts(categoryName).subscribe((res:any) => {
+      this.products = res
+      console.log(this.products);
+    },
+    err => {alert(err.message);
+    }
+    )
+  }
+
 }
